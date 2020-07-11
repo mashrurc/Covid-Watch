9238ed12348b617ad9224bb97471723c10450836
Mahbod <mahbodsabbaghi@gmail.com>
2020-07-11 17:45:09 -0400

    -Added Landing Page, still work in progress

---
+++ landingPage1/landingPage1.jsx
@@ -0,0 +1,62 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./landingPage1.css";
+
+class LandingPage1 extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="83e921c2-8d1f-4734-a10c-601548400707" className="landingPage1">        <div data-layer="80ff3cc3-b34f-45c2-b48b-a9ebd25cc12f" className="landingPage1Fba83fae5eda"></div>
+        <svg data-layer="d758d2af-55c5-469f-a9b1-6307102631fa" preserveAspectRatio="none" viewBox="-30 -30 460 460" className="ellipse29"><path d="M 200 0 C 310.4569396972656 0 400 89.54306030273438 400 200 C 400 310.4569396972656 310.4569396972656 400 200 400 C 89.54306030273438 400 0 310.4569396972656 0 200 C 0 89.54306030273438 89.54306030273438 0 200 0 Z"  /></svg>
+        <svg data-layer="577b70c7-f156-4f2f-88d9-3f6fae6bd2dc" preserveAspectRatio="none" viewBox="-17.5 -17.5 35 99" className="line26"><path d="M 0 0 L 0 64"  /></svg>
+        <svg data-layer="bdbace1e-754b-489c-998d-e60df6a315ed" preserveAspectRatio="none" viewBox="-24.508056640625 -24.508056640625 118.01611328125 101.01611328125" className="line30"><path d="M 69 52 L 0 0"  /></svg>
+        <svg data-layer="fc0f1836-1156-43f9-ab4c-9ac0b48c1a83" preserveAspectRatio="none" viewBox="-23.822509765625 -23.822509765625 84.64501953125 113.64501953125" className="line31"><path d="M 37 66 L 0 0"  /></svg>
+        <svg data-layer="7f335ead-ce71-420d-875b-d450a8cbba6a" preserveAspectRatio="none" viewBox="-17.712158203125 -17.71209716796875 36.42431640625 117.4241943359375" className="line32"><path d="M 1 82 L 0 0"  /></svg>
+        <svg data-layer="cb14e12a-0809-44cf-ad2c-0de8fc89cc20" preserveAspectRatio="none" viewBox="-24.206787109375 -24.2069091796875 85.41357421875 105.413818359375" className="line33"><path d="M 0 57 L 37 0"  /></svg>
+        <svg data-layer="b20bfacd-7085-40e1-935e-0f0b215e775c" preserveAspectRatio="none" viewBox="-23.837890625 -23.837890625 102.67578125 78.67578125" className="line34"><path d="M 0 31 L 55 0"  /></svg>
+        <svg data-layer="3031b817-4742-4d2f-b362-dda171bcab63" preserveAspectRatio="none" viewBox="-17.937255859375 -17.93731689453125 114.87451171875 37.8746337890625" className="line35"><path d="M 0 0 L 79 2"  /></svg>
+        <svg data-layer="1d026436-1a21-42e4-a046-adbdfa7e7a18" preserveAspectRatio="none" viewBox="-24.508056640625 -24.508056640625 118.01611328125 101.01611328125" className="line36"><path d="M 0 0 L 69 52"  /></svg>
+        <svg data-layer="818237a3-142e-4fae-8770-d97f86793233" preserveAspectRatio="none" viewBox="-23.822509765625 -23.822509765625 84.64501953125 113.64498901367188" className="line37"><path d="M 0 0 L 37 66"  /></svg>
+        <svg data-layer="a657352f-58c8-4df5-8037-149b1b06ce43" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse30"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="886e34b4-54b5-44b4-938f-b314b3c43646" preserveAspectRatio="none" viewBox="0 0 60 60" className="ellipse32"><path d="M 30 0 C 46.56854248046875 0 60 13.43145942687988 60 30 C 60 46.56854248046875 46.56854248046875 60 30 60 C 13.43145942687988 60 0 46.56854248046875 0 30 C 0 13.43145942687988 13.43145942687988 0 30 0 Z"  /></svg>
+        <svg data-layer="2410a3f6-55d3-44a4-b111-99a966f983de" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse36"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="6514b6af-0bb2-4601-b341-75c805f613fc" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse37"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="d8d77f06-c183-4a52-8205-f59a6049df08" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse38"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="b0e0953b-5f32-49fd-a4b0-ccc40c23648b" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse39"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="d1012a82-06fb-4905-a88f-0eab91910701" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse40"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="b8340d1e-580e-4ea0-ad72-04c6183adcd1" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse41"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="ff5c51fc-edac-4231-8c98-16bccd9c607f" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse42"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="e3a2e213-4bf7-42a9-a51d-f3da96a8f48f" preserveAspectRatio="none" viewBox="-24.206787109375 -24.2069091796875 85.41357421875 105.413818359375" className="line27"><path d="M 37 0 L 0 57"  /></svg>
+        <svg data-layer="7129b305-a749-4745-9e0c-98d82cd3fbd6" preserveAspectRatio="none" viewBox="-23.837890625 -23.837890625 102.67578125 78.67578125" className="line28"><path d="M 55 0 L 0 31"  /></svg>
+        <svg data-layer="ec811064-4cdc-41d5-94fe-f3534b902e92" preserveAspectRatio="none" viewBox="-17.937255859375 -17.93731689453125 114.87451171875 37.8746337890625" className="line29"><path d="M 79 2 L 0 0"  /></svg>
+        <svg data-layer="ceccacaa-bb01-4d66-afec-87bd7ed2404d" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse31"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="a6f1d69f-80fe-46af-8c84-4e1cb097a0bf" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse33"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="92e7187c-dd0c-44c1-b4ec-04679d17f3a0" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse34"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+        <svg data-layer="aa9f2268-a13c-4d53-b3cc-1a76fe2b6680" preserveAspectRatio="none" viewBox="0 0 50 50" className="ellipse35"><path d="M 25 0 C 38.8071174621582 0 50 11.1928825378418 50 25 C 50 38.8071174621582 38.8071174621582 50 25 50 C 11.1928825378418 50 0 38.8071174621582 0 25 C 0 11.1928825378418 11.1928825378418 0 25 0 Z"  /></svg>
+</div>
+
+    );
+  }
+}
+
+LandingPage1.propTypes = {
+
+}
+
+LandingPage1.defaultProps = {
+
+}
+
+
+export default LandingPage1;
+          
