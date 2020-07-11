e99dd74bbd51b8fb0a5ab1cbee7755e2bd5cc64d
Mahbod <mahbodsabbaghi@gmail.com>
2020-07-11 13:07:27 -0400

    -Added landing page, survey page, and dashboard

---
+++ landingPage/landingPage.jsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./landingPage.css";
+
+class LandingPage extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="f906af77-533d-42ad-8d2d-efa1b2b82fe7" className="landingPage">        <div data-layer="a794d2a2-c0f8-43c7-9ca0-ddee74f641f4" className="landingPage40591427">Landing Page</div>
+</div>
+
+    );
+  }
+}
+
+LandingPage.propTypes = {
+
+}
+
+LandingPage.defaultProps = {
+
+}
+
+
+export default LandingPage;
+          
