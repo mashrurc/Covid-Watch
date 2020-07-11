7704197edadf1728b3f2cf60f7378bafc485c898
Mahbod <mahbodsabbaghi@gmail.com>
2020-07-11 11:48:47 -0400

    -Init push

---
+++ indexPage/indexPage.js
@@ -0,0 +1,126 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import {StyleSheet, Text, View, TextInput, FlatList, Picker, ScrollView, TouchableHighlight} from 'react-native';
+import {Image as ReactImage} from 'react-native';
+
+
+export default class IndexPage extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          
+      };
+  }
+
+
+  handlePress(target, owner) {
+    if (this.props.onPress) {
+        let name;
+        let id;
+        let index = -1;
+        if (target.search("::") > -1) {
+            const varCount = target.split("::").length;
+            if (varCount === 2) {
+                name = target.split("::")[0];
+                id = target.split("::")[1];
+            } else if (varCount === 3) {
+                name = target.split("::")[0];
+                index = parseInt(target.split("::")[1]);
+                id = target.split("::")[2];
+            }
+        } else {
+            name = target;
+        }
+        this.props.onPress({ type: 'button', name: name, index: index, id: id, owner: owner });
+    }
+  }
+
+  handleChangeTextinput(name, value) {
+      let id;
+      let index = -1;
+      if (name.search('::') > -1) {
+          const varCount = name.split("::").length;
+          if (varCount === 2) {
+              name = name.split("::")[0];
+              id = name.split("::")[1];
+          } else if (varCount === 3) {
+              name = name.split("::")[0];
+              index = name.split("::")[1];
+              id = name.split("::")[2];
+          }
+      } else {
+          name = name;
+      }
+      let state = this.state;
+      state[name.split('::').join('')] = value;
+      this.setState(state, () => {
+          if (this.props.onChange) {
+              this.props.onChange({ type: 'textinput', name: name, value: value, index: index, id: id });
+          }
+      });
+  }
+
+  render() {
+    
+    return (
+    <ScrollView data-layer="f906af77-533d-42ad-8d2d-efa1b2b82fe7" style={styles.indexPage}>
+        <Text data-layer="a794d2a2-c0f8-43c7-9ca0-ddee74f641f4" style={styles.indexPage_landingPage}>Landing Page</Text>
+    </ScrollView>
+    );
+  }
+}
+
+IndexPage.propTypes = {
+
+}
+
+IndexPage.defaultProps = {
+
+}
+
+
+const styles = StyleSheet.create({
+  "indexPage": {
+    "opacity": 1,
+    "position": "relative",
+    "backgroundColor": "rgba(255, 255, 255, 1)",
+    "marginTop": 0,
+    "marginRight": 0,
+    "marginBottom": 0,
+    "marginLeft": 0,
+    "paddingTop": 0,
+    "paddingRight": 0,
+    "paddingBottom": 0,
+    "paddingLeft": 0,
+    "width": "auto",
+    "height": "auto",
+    "left": 0,
+    "top": 0,
+    "right": 0,
+    "bottom": 0
+  },
+  "indexPage_landingPage": {
+    "opacity": 1,
+    "position": "absolute",
+    "backgroundColor": "rgba(255, 255, 255, 0)",
+    "color": "rgba(112, 112, 112, 1)",
+    "fontSize": 50,
+    "fontWeight": "400",
+    "fontStyle": "normal",
+    "fontFamily": "Segoe UI",
+    "textAlign": "center",
+    "marginTop": 0,
+    "marginRight": 0,
+    "marginBottom": 0,
+    "marginLeft": 0,
+    "paddingTop": 0,
+    "paddingRight": 0,
+    "paddingBottom": 0,
+    "paddingLeft": 0,
+    "width": 798,
+    "height": 131,
+    "left": 471,
+    "top": 91
+  }
+});
---
+++ indexPage/indexPage.jsx
@@ -0,0 +1,36 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./indexPage.css";
+
+class IndexPage extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  }
+  
+  render() {
+    
+    return (
+          <div data-layer="f906af77-533d-42ad-8d2d-efa1b2b82fe7" className="indexPage">        <div data-layer="a794d2a2-c0f8-43c7-9ca0-ddee74f641f4" className="landingPage">Landing Page</div>
+</div>
+
+    );
+  }
+}
+
+IndexPage.propTypes = {
+
+}
+
+IndexPage.defaultProps = {
+
+}
+
+
+export default IndexPage;
+          
